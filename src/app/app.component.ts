12031f1578390f412caff62bc4d8c0f585c4a988
Seth Condie <seth.condie@lovelandinnovations.com>
2023-11-17 10:40:24 -0700

    Included a container and a button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {ContainerComponent} from "./components/container/container.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'the-game-pensive-web';
+  title: string = 'the-game-pensive-web';
 }
