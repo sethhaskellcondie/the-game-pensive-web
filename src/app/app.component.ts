dd263c54ba02842ec9c3eb43ea72c869bf2e5819
Seth Condie <seth.condie@lovelandinnovations.com>
2023-11-19 12:27:27 -0700

    removed the experiment components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {ContainerComponent} from "./components/container/container.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
